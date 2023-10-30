8e13f07f92ae574f3e82fa0d12d1ca2449295c48
leath-dub <fierceinbattle@gmail.com>
2023-10-30 19:17:38 +0000

    init

--- flake.nix
+++ flake.nix
@@ -1,21 +1,17 @@
 {
   description = "Simple flake for directories";
 
-  inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  };
-
-  outputs = { self, nixpkgs, systems }: let
+  outputs = { self }: let
     in {
-      lib.rawDir = src: derivation {
+      lib.rawDir = { src, pkgs }: derivation {
         name = "conf";
 
-        builder = "${nixpkgs.busybox}/bin/busybox";
+        builder = "${pkgs.busybox}/bin/busybox";
         args = [ "sh" ./builder.sh ];
 
         system = builtins.currentSystem;
 
-        inherit (nixpkgs) busybox;
+        inherit (pkgs) busybox;
         inherit src;
       };
     };
